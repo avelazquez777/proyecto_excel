6f1b8a1b8454a3a20637bb72f95118f82463239f
avelazquez777 <a.velazquez@itecriocuarto.org.ar>
2025-09-09 00:09:10 -0300

    Corregir wsg

--- aranceles/wsgi.py
+++ aranceles/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for araceles project.
+WSGI config for aranceles project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'araceles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aranceles.settings')
 
 application = get_wsgi_application()
